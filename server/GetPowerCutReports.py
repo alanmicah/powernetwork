47eba5587db159aa32f29ca04c8761f5f0d31b9a
Alan D <alanfding@gmail.com>
2022-08-30 17:43:07 +0100

    Success: Retrieve and upload live reports

--- server/GetPowerCutReports.py
+++ server/GetPowerCutReports.py
@@ -4,6 +4,11 @@ PowerCutReports.py created by Alan D 04/08/2022
 Reads webapge and parses the relevant elements for the live report.
 This script should be ran as a heartbeat (executing constantly every minute or so)
 """
+
+"""
+Referencings
+[1]https://www.geeksforgeeks.org/scrape-content-from-dynamic-websites/
+"""
 import asyncio, json, requests, urllib.request, datetime, time
 from extensions import db
 from models import PowerCutReports
@@ -17,7 +22,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 """
 Render and parse dynamic page content
 """
-def dynamic_reports():
+def get_dynamic_reports():
+  #[1]
   try:
     url = "https://www.ukpowernetworks.co.uk/power-cut/list"
     
@@ -25,8 +31,8 @@ def dynamic_reports():
     driver.get(url) 
       
     # this is just to ensure that the page is loaded
-    time.sleep(5) 
-      
+    # time.sleep(0) 
+  
     html = driver.page_source
     
     # this renders the JS code and stores all
@@ -35,14 +41,101 @@ def dynamic_reports():
     # Now, we could simply apply bs4 to html variable
     soup = BeautifulSoup(html, "html.parser")
 
-    savePage = soup.contents
-
-    with open('data/ReportsRender.html', 'w', encoding='utf-8') as f:
-      f.write(str(soup))
+    # savePage = soup.contents
 
   except Exception as e:
     print(e)
     print('Failed to render webpage')
+  
+  try:
+    # Parsing only elements found in the table div class
+    tableBody = soup.find("div", {"class": "PowerCutList_PowerCutListWrapper__dy74M"})
+    textBody = tableBody.find_all("div", {"class": "PowerCutListItem_PowerCutListItem__AzYtO"})
+    
+  except Exception as e:
+    print(e)
+    print('Unable to find div class')
+
+  try:
+    data = []
+
+    for row in textBody:
+      cols = row.find_all('p')
+      # Not all necessary text are found in <p>
+      postcodes = row.find("div", {"class": "PowerCutListItem_PowerCutPostcodes__xlT_j"})
+      affected = row.find("div", {"class": "PowerCutListItem_PowerCutCustomers__UUhFc"})
+      
+      cols = [ele.text.strip() for ele in cols]
+      postcodes = [ele.text.strip() for ele in postcodes]
+      affected = [ele.text.strip() for ele in affected]
+      
+      # This element contains the amount of reported affected customers,
+      # Converts element to an int() if it contains a value
+      if (affected[1] != '-'):
+        affected[1] = int(affected[1])
+      else:
+        affected[1] = 0
+      
+      cols = cols + postcodes + affected
+      
+      # Each element in 'unwanted' is used as an index of elements
+      # Remove unnecessary elements at each index position found in 'unwanted'
+      unwanted = [0,2,3,7,8,10,12]
+      for ele in sorted(unwanted, reverse = True):
+        del cols[ele]
+      
+      data.append(cols)
+    with open('data/live_reports.json', 'w') as filehandle:
+      json.dump(data, filehandle)
+        
+  except Exception as e:
+    print(e)
+    print('Unable to extract <p> columns')
+
+  # Upload retrieved data in json file to database
+  try:
+    networkTable = db.session.query(PowerCutReports)
+  except Exception as e:
+    print(e)
+    print('Failed to get from PowerCutReports')
+    return 'Failure'
+  
+  reportsJson = open('data/live_reports.json', 'r')
+  eachReports = json.load(reportsJson)
+
+  try:
+    for items in eachReports:
+        powercutreports = PowerCutReports()
+        powercutreports.id = items[4]
+        powercutreports.type = items[0]
+        powercutreports.postcodes = items[5]
+        powercutreports.restoretime = items[1]
+        powercutreports.information = items[2]
+        powercutreports.starttime = items[3]
+        powercutreports.reports = items[6]
+        powercutreports.lastupdate = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        if networkTable is not None:
+          db.session.merge(powercutreports)
+        else:
+          db.session.add(powercutreports)
+
+  except Exception as e:
+    db.session.rollback()
+    print(e)
+    print('Failed to upload Power Cut Reports')
+  
+  try:
+    db.session.commit()
+    print('Success')
+    return 'Success'
+  except Exception as e:
+    db.session.rollback()
+    print(e)
+    print('Failed to upload')
+    return str(e)
+  finally:
+    db.session.close()  
 
 
 """
@@ -180,6 +273,6 @@ def remove_old_reports():
 
 ###
 #-----Function calls-----#
-# remove_old_reports()
+remove_old_reports()
 # try_live_reports()
-dynamic_reports()
+get_dynamic_reports()
