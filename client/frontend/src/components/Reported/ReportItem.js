12f83997582872b3fd57ca0c6a0c4fcaef055c1a
Alan D <31675641+alanmicah@users.noreply.github.com>
2022-09-26 20:59:02 +0100

    evening update

--- client/frontend/src/components/Reported/ReportItem.js
+++ client/frontend/src/components/Reported/ReportItem.js
@@ -1,15 +1,16 @@
 import './ReportItem.scss';
 import Card from '../UI/Card';
-import ReportDate from './ReportDate';
+// import ReportDate from './ReportDate';
 
 const ReportItem = (props) => {
   return (
     <li>
       <Card className='report-item'>
-        <ReportDate date={props.date} />
+        {/* <ReportDate date={props.date} /> */}
         <div className='report-item__description'>
           <h2>{props.postcode}</h2>
-          <div className='report-item__price'>{props.description}</div>
+          <div className='report-item__price'>{props.type}</div>
+          <h3>{props.restore}</h3>
         </div>
       </Card>
     </li>
--- client/frontend/src/components/Reported/ReportsList.js
+++ client/frontend/src/components/Reported/ReportsList.js
@@ -1,6 +1,23 @@
 import ReportItem from './ReportItem';
 import './ReportsList.scss';
 
-function ReportsList(props) {}
+function ReportsList(props) {
+  if (props.items.length === 0) {
+    return <h2 className='reports-list__fallback'>Found no reports</h2>;
+  }
+
+  return (
+    <ul className='reports-list'>
+      {props.items.map((report) => (
+        <ReportItem
+          key={report.id}
+          type={report.type}
+          postcodes={report.postcodes}
+          restore={report.restoretime}
+        />
+      ))}
+    </ul>
+  );
+}
 
 export default ReportsList;
